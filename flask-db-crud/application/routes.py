7be947170fca4df04e391db2d662d03160761eb1
Rebekah Akingbala <r.akingbala52@gmail.com>
2022-02-08 15:58:07 +0000

    commiting all the route changes and new gitignore file

---
+++ flask-db-crud/application/routes.py
@@ -0,0 +1,31 @@
+from application import app, db
+from application.models import Games
+
+@app.route('/add')
+def add():
+    new_game = Games(name="New Game")
+    db.session.add(new_game) #'create' functionality
+    db.session.commit()
+    return "Added new game to database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>"+ game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
+
+@app.route('delete')
+def delete(name):
+    first_game = Games.query.get(1)
+    db.session.delete(first_game)
+    db.session.commit()
+    return "You have deleted the first game in the database."
