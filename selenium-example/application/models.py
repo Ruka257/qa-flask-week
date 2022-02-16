b116d18d40e42006a20268f073a8486e1d6683c5
Ubuntu <rebekah@development-server.dq2pivvzhwverptj4qj50lbwdh.zx.internal.cloudapp.net>
2022-02-16 10:02:57 +0000

    changed selenium-example models

--- selenium-example/application/models.py
+++ selenium-example/application/models.py
@@ -2,4 +2,4 @@ from application import db
 
 class Games(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
+    name = db.Column(db.String(30), nullable=False)
